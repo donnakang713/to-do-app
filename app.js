760ec76e80edcaaa7e0ac2b019ca51a74b1966ae
Donna <donnakang713@gmail.com>
2018-03-13 19:48:25 -0700

    Assignment 5 First Draft

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 function onReady() {
+  let id = 0;
   const toDos =[];
   const addToDoForm = document.getElementById('addToDoForm');
 
@@ -9,28 +10,39 @@ function onReady() {
     toDos.push ({
       title: newToDoText.value,
       complete: false
+      id: id.value;
     });
+    id=id+1;
     newToDoText.value='';
-
     renderTheUI();
   }
 
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
-
+      const deleteButton= document.createElement('button');
+      deleteButton.type = "button";
       newLi.textContent = toDo.title;
-
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+    });
+
+    deleteButton.addEventListener ('click', event => {
+      event.preventDefault();
+      deleteToDoList(toDo);
+      renderTheUI();
     });
   }
+
+function deleteToDoList (toDo){
+  let deleteToDo = toDos.filter (toDo => toDo.id);
+}
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
@@ -38,8 +50,8 @@ function onReady() {
   });
 
   renderTheUI();
-}
+
 
 window.onload = function() {
    onReady();
-};
+ };
